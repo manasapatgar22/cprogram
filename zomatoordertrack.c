097781ecd509e9cf0d664a3e3f090af47f72b14f
Manasa Mohan Patgar <mohanpatgar120@gmail.com>
2025-02-18 22:57:27 +0530

    Update zomatoordertrack.c

--- zomatoordertrack.c
+++ zomatoordertrack.c
@@ -1,3 +1,37 @@
+/* Payment Gateway Simulation
+Problem Statement:
+A payment gateway processes online
+transactions with the following steps:
+1.
+User
+selects a payment method:
+1 for
+2 for
+3 for
+If
+the user selects an invalid option, show an error message.
+If a
+valid method is chosen, the system asks for further details:
+For ,
+enter a 16-digit card number.
+For , enter a
+valid UPI ID (e.g., user@upi).
+For ,
+select a bank from predefined options (e.g., 1 for SBI, 2 for HDFC).
+If
+the details are correct, display "Payment
+Successful!"
+Otherwise,
+prompt "Transaction Failed. Please
+try again."
+Select Payment Method
+(1-3): 2
+Enter your UPI ID: user@upi
+Payment Successful!
+Select Payment Method
+(1-3): 5
+Invalid payment method. Please try again.*/
+
 #include <stdio.h>
 
 int main() {
